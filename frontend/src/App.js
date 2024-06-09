22c659b26bb5e5db1b72ad120d525dca0eed3051
Kuldeep Rajeshbhai Gajera <kl210309@dal.ca>
2024-06-09 14:49:12 -0300

    Revert "Merge branch 'registerPage' into 'main'"
    This reverts merge request !1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
-import { BrowserRouter } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/navbar/NavBar';
-import { Router } from './Router/Router';
 
 function App() {
-  
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Router />
-    </BrowserRouter>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
