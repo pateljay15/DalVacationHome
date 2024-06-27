b0d19a3ae1b823777aeca989d57c181e21ebf954
Jeet Jani <jeetjani@dal.ca>
2024-06-27 10:28:27 -0300

    feat: integrate the Dialogflow chatbot with the landing page

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -6,6 +6,9 @@ const LandingPage = () => {
       <div className="p-10 bg-white shadow-lg rounded-lg">
         <h1 className="text-2xl font-semibold">Welcome to Book Me !</h1>
       </div>
+      
+      <iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/09dddef5-582f-4703-acd3-3f7ee1b45c11"></iframe>
+
     </div>
   );
 }
