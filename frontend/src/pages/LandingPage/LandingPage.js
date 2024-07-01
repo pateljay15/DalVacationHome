a685b73ad8650cfd72005673a8a327f345586e46
Christin Saji <ch580848@dal.ca>
2024-06-30 23:23:21 -0300

    update chatbot styling

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -1,16 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const LandingPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="p-10 bg-white shadow-lg rounded-lg">
-        <h1 className="text-2xl font-semibold">Welcome to Book Me !</h1>
+        <h1 className="text-2xl font-semibold">Welcome to Book Me!</h1>
       </div>
-      
-      <iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/09dddef5-582f-4703-acd3-3f7ee1b45c11"></iframe>
-
     </div>
   );
-}
+};
 
 export default LandingPage;
