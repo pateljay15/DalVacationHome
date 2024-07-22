9103b760040a2533e8eaf7bf760587241e9b40f8
Christin Saji <ch580848@dal.ca>
2024-07-21 21:30:53 -0300

    add second chatbot terraform script

--- Infrastructure as code (IaC) files/GCP/Virtual Bot/main.tf
+++ Infrastructure as code (IaC) files/GCP/Virtual Bot/main.tf
@@ -5,9 +5,16 @@ resource "google_dialogflow_agent" "guest_agent" {
   description           = "An example Dialogflow agent"
 }
 
+resource "google_dialogflow_agent" "vacbot_agent" {
+  display_name          = "vacbot-agent"
+  default_language_code = "en"
+  time_zone             = "America/Los_Angeles"
+  description           = "Dialogflow agent for authenticate users"
+}
+
 resource "google_dialogflow_intent" "booking_navigation" {
   display_name = "Booking Navigation"
-  depends_on   = [google_dialogflow_agent.guest_agent]
+  depends_on   = [google_dialogflow_agent.guest_agent, google_dialogflow_agent.vacbot_agent]
 
   training_phrases {
     parts {
@@ -30,7 +37,7 @@ resource "google_dialogflow_intent" "booking_navigation" {
 
 resource "google_dialogflow_intent" "default_fallback_intent" {
   display_name = "Default Fallback Intent"
-  depends_on   = [google_dialogflow_agent.guest_agent]
+  depends_on   = [google_dialogflow_agent.guest_agent, google_dialogflow_agent.vacbot_agent]
 
   is_fallback = true
 
@@ -56,7 +63,7 @@ resource "google_dialogflow_intent" "default_fallback_intent" {
 
 resource "google_dialogflow_intent" "default_welcome_intent" {
   display_name = "Default Welcome Intent"
-  depends_on   = [google_dialogflow_agent.guest_agent]
+  depends_on   = [google_dialogflow_agent.guest_agent, google_dialogflow_agent.vacbot_agent]
 
   events = ["WELCOME"]
 
@@ -134,7 +141,7 @@ resource "google_dialogflow_intent" "default_welcome_intent" {
 
 resource "google_dialogflow_intent" "homepage_navigation" {
   display_name = "Homepage Navigation"
-  depends_on   = [google_dialogflow_agent.guest_agent]
+  depends_on   = [google_dialogflow_agent.guest_agent, google_dialogflow_agent.vacbot_agent]
 
   training_phrases {
     parts {
@@ -185,7 +192,7 @@ resource "google_dialogflow_intent" "homepage_navigation" {
 
 resource "google_dialogflow_intent" "login_navigation" {
   display_name = "Login Navigation"
-  depends_on   = [google_dialogflow_agent.guest_agent]
+  depends_on   = [google_dialogflow_agent.guest_agent, google_dialogflow_agent.vacbot_agent]
 
   training_phrases {
     parts {
@@ -251,7 +258,7 @@ resource "google_dialogflow_intent" "login_navigation" {
 
 resource "google_dialogflow_intent" "mybookings_navigation" {
   display_name = "MyBookings Navigation"
-  depends_on   = [google_dialogflow_agent.guest_agent]
+  depends_on   = [google_dialogflow_agent.guest_agent, google_dialogflow_agent.vacbot_agent]
 
   training_phrases {
     parts {
@@ -308,8 +315,8 @@ resource "google_dialogflow_intent" "mybookings_navigation" {
 
 resource "google_dialogflow_intent" "signup_navigation" {
   display_name = "Signup Navigation"
-  depends_on   = [google_dialogflow_agent.guest_agent]
-
+  depends_on   = [google_dialogflow_agent.guest_agent, google_dialogflow_agent.vacbot_agent]
+  
   training_phrases {
     parts {
       text = "How do I register?"
@@ -384,7 +391,7 @@ resource "google_dialogflow_intent" "signup_navigation" {
 
 resource "google_dialogflow_intent" "welcome_intent" {
   display_name = "Welcome"
-  depends_on   = [google_dialogflow_agent.guest_agent]
+  depends_on   = [google_dialogflow_agent.guest_agent, google_dialogflow_agent.vacbot_agent]
 
   events = ["vacbot_welcome"]
 
@@ -422,5 +429,315 @@ resource "google_dialogflow_intent" "welcome_intent" {
     text {
       text = ["Welcome to DalVacationHome! How can I assist you today?"]
     }
+  }
+}
+
+resource "google_dialogflow_intent" "booking_info_intent" {
+  display_name = "Booking Info Intent"
+  depends_on   = [google_dialogflow_agent.vacbot_agent]
+
+  training_phrases {
+    parts {
+      text        = "BH123456"
+      entity_type = "@BookingReference"
+      alias       = "BookingReferenceCode"
+    }
+  }
+
+  training_phrases {
+    parts {
+      text = "Room number?"
+    }
+  }
+
+  training_phrases {
+    parts {
+      text = "What's my room number?"
+    }
+  }
+
+  training_phrases {
+    parts {
+      text = "This is my booking reference code "
+    }
+    parts {
+      text        = "BH123456"
+      entity_type = "@BookingReference"
+      alias       = "BookingReferenceCode"
+    }
+    parts {
+      text = ". Could you please fetch me my booking details?"
+    }
+  }
+
+  parameters {
+    display_name             = "BookingReferenceCode"
+    entity_type_display_name = "@BookingReference"
+    value                    = "$BookingReferenceCode"
+    mandatory                = true
+    prompts                  = ["What's your Booking Reference Code?"]
+  }
+
+  messages {
+    text {
+      text = ["Fetching your booking details for reference code ${BookingReferenceCode}."]
+    }
+  }
+
+  fulfillment {
+    webhook {
+      enabled = true
+    }
+  }
+}
+
+resource "google_dialogflow_intent" "customer_support_request_intent" {
+  display_name = "Customer Support Request Intent"
+  depends_on   = [google_dialogflow_agent.vacbot_agent]
+
+  training_phrases {
+    parts {
+      text = "Can I get connected to an agent? "
+    }
+    parts {
+      text        = "I am having trouble with booking a room"
+      entity_type = "@sys.any"
+      alias       = "Issue"
+    }
+    parts {
+      text = ". My booking reference is "
+    }
+    parts {
+      text        = "BH557834."
+      entity_type = "@BookingReference"
+      alias       = "BookingReferenceCode"
+    }
+  }
+
+  training_phrases {
+    parts {
+      text = "Please connect me to an agent. This is my booking code: "
+    }
+    parts {
+      text        = "BH123456"
+      entity_type = "@BookingReference"
+      alias       = "BookingReferenceCode"
+    }
+  }
+
+  training_phrases {
+    parts {
+      text = "I need to talk to an agent"
+    }
+  }
+
+  training_phrases {
+    parts {
+      text = "I would like to get connected with an agent"
+    }
+  }
+
+  parameters {
+    display_name             = "Issue"
+    entity_type_display_name = "@sys.any"
+    value                    = "$Issue"
+    mandatory                = true
+    prompts                  = ["Please describe your issue."]
+  }
+
+  parameters {
+    display_name             = "BookingReferenceCode"
+    entity_type_display_name = "@BookingReference"
+    value                    = "$BookingReferenceCode"
+    mandatory                = true
+    prompts                  = ["What's your booking reference code?"]
+  }
+
+  parameters {
+    display_name             = "Email"
+    entity_type_display_name = "@sys.email"
+    value                    = "$Email"
+    mandatory                = true
+    prompts                  = ["Please enter your email ID"]
+  }
+
+  messages {
+    text {
+      text = ["Your query has been submitted. An agent will be contacting you soon."]
+    }
+  }
+
+  fulfillment {
+    webhook {
+      enabled = true
+    }
+  }
+}
+
+resource "google_dialogflow_intent" "hotel_booking_intent" {
+  display_name = "Hotel Booking Intent"
+  depends_on   = [google_dialogflow_agent.vacbot_agent]
+
+  training_phrases {
+    parts {
+      text        = "book a room on "
+    }
+    parts {
+      text        = "20th July"
+      entity_type = "@sys.date"
+      alias       = "CheckinDate"
+    }
+    parts {
+      text        = " for "
+    }
+    parts {
+      text        = "3"
+      entity_type = "@sys.number"
+      alias       = "Guests"
+    }
+    parts {
+      text        = " people in a "
+    }
+    parts {
+      text        = "family"
+      entity_type = "@RoomType"
+      alias       = "RoomType"
+    }
+    parts {
+      text        = " sized room in "
+    }
+    parts {
+      text        = "Mumbai"
+      entity_type = "@sys.geo-city"
+      alias       = "City"
+    }
+    parts {
+      text        = " for "
+    }
+    parts {
+      text        = "4"
+      entity_type = "@sys.number"
+      alias       = "Nights"
+    }
+    parts {
+      text        = " nights"
+    }
+  }
+
+  training_phrases {
+    parts {
+      text = "Book a room"
+    }
+  }
+
+  training_phrases {
+    parts {
+      text = "Book hotel"
+    }
+  }
+
+  parameters {
+    display_name             = "City"
+    entity_type_display_name = "@sys.geo-city"
+    value                    = "$City"
+    mandatory                = true
+    prompts                  = ["Which city are you planning to visit?"]
+  }
+
+  parameters {
+    display_name             = "Guests"
+    entity_type_display_name = "@sys.number"
+    value                    = "$Guests"
+    mandatory                = true
+    prompts                  = ["For how many people do you need a room?"]
+  }
+
+  parameters {
+    display_name             = "Nights"
+    entity_type_display_name = "@sys.number"
+    value                    = "$Nights"
+    mandatory                = true
+    prompts                  = ["How many nights are you planning to stay?"]
+  }
+
+  parameters {
+    display_name             = "CheckinDate"
+    entity_type_display_name = "@sys.date"
+    value                    = "$CheckinDate"
+    mandatory                = true
+    prompts                  = [
+      "When can we expect you?",
+      "When will you be checking in?"
+    ]
+  }
+
+  parameters {
+    display_name             = "RoomType"
+    entity_type_display_name = "@RoomType"
+    value                    = "$RoomType"
+    mandatory                = true
+    prompts                  = ["What type of room would you like to book? Available options are: Single, Double, Twin, Family, Suite, Deluxe, and Accessible"]
+  }
+
+  messages {
+    text {
+      text = ["Alright, I have you down for a ${RoomType} room booked for ${Nights} nights in ${City} for ${Guests} people. We are expecting you on ${CheckinDate}. See ya soon!"]
+    }
+  }
+
+  fulfillment {
+    webhook {
+      enabled = true
+    }
+  }
+}
+
+resource "google_dialogflow_entity_type" "booking_reference" {
+  display_name = "BookingReference"
+  kind         = "KIND_MAP"
+
+  entities {
+    value = "BH\\d{6}"
+    synonyms = ["BH\\d{6}"]
+  }
+}
+
+resource "google_dialogflow_entity_type" "room_type" {
+  display_name = "RoomType"
+  kind         = "KIND_MAP"
+
+  entities {
+    value = "Single"
+    synonyms = ["Single"]
+  }
+
+  entities {
+    value = "Double"
+    synonyms = ["Double"]
+  }
+
+  entities {
+    value = "Twin"
+    synonyms = ["Twin"]
+  }
+
+  entities {
+    value = "Family"
+    synonyms = ["Family"]
+  }
+
+  entities {
+    value = "Suite"
+    synonyms = ["Suite"]
+  }
+
+  entities {
+    value = "Deluxe"
+    synonyms = ["Deluxe"]
+  }
+
+  entities {
+    value = "Accessible"
+    synonyms = ["Accessible"]
   }
 }
